922b804457f336e428c5107e8bbd426d93cd163d
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-04-13 23:16:27 -0300

    Update Area_dCirculo.cpp

--- Area_dCirculo.cpp
+++ Area_dCirculo.cpp
@@ -4,20 +4,13 @@
 float areaCirculo(float raio){
 	return M_P * pow(raio, 2);
 }
-float superficieEsfera(float raio){
-	return 4 * M_P * pow(raio, 2);
-}
+
 int main(){
 	int areaCirc=0;
     float raio=0;	
 	printf("Digite o raio da circuferencia:\n");
 	scanf("%f", &raio);
 	printf("\nArea do circulo= %.4f\n", areaCirculo(raio));
-	
-	printf("Digite o raio da esfera:\n");
-	scanf("%f", &raio);
-	printf("\nArea da superficie esfera= %.4f\n", superficieEsfera(raio));
-	
 	
     return 0;   
 }
