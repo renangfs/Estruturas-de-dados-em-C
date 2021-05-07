495462a682c2b963c493c0fcc11381fdd05eefcb
Renan Germano <61218420+renangfs@users.noreply.github.com>
2021-05-07 11:44:59 -0300

    Update 3parametros.cpp

--- 3parametros.cpp
+++ 3parametros.cpp
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-float reajust_sal(float ant_sal,float porcent,float *val_reajust){
-	 *val_reajust = ant_sal;
-	 ant_sal = porcent * (ant_sal/100);	
-	 *val_reajust = *val_reajust + ant_sal;
+float reajust_sal(float antigo_sal,float porcentual,float *val_reajust){
+	 *val_reajust = antigo_sal;
+	 antigo_sal = porcentual * (antigo_sal/100);	
+	 *val_reajust = *val_reajust + antigo_sal;
 }
 int main(){
-	float ant_sal,porcent,val_reajust=0;
-    printf("salário a ser reajustado:");
-    scanf("%f", &ant_sal);
-    printf("porcentual de reajuste:");
-    scanf("%f", &porcent);
-    reajust_sal(ant_sal,porcent,&val_reajust);
+	float antigo_sal,porcentual,val_reajust=0;
+    	printf("salario a ser reajustado:");
+    	scanf("%f", &antigo_sal);
+    	printf("porcentual de reajuste:");
+    	scanf("%f", &porcentual);
+    	reajust_sal(antigo_sal,porcentual,&val_reajust);
     
-    printf("Salario reajustado:%.2f", val_reajust);
+   	 printf("\nSalario reajustado:%.2f", val_reajust);
     
 }
